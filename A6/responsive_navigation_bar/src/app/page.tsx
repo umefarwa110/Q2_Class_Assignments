0536a5356b6ed0cc9825ca3ca882df135308e5e0
umefarwa110 <umefarwarind101@gmail.com>
2024-11-27 17:00:51 +0500

    q2_class_assignments

--- A6/responsive_navigation_bar/src/app/page.tsx
+++ A6/responsive_navigation_bar/src/app/page.tsx
@@ -2,13 +2,36 @@ export default function Home() {
   return (
     <>
 
-      <div className="text-center  text-white text-2xl items-center m-[14px] ">
-        <div className="bg-red-500 h-[120px] " >First use of Tailwind :)</div>
-      </div>
+      <div className="
+        text-center 
+        text-white 
+        text-2xl 
+        m-[14px] 
+        
 
-      <div className=" flex flex-row justify-center text-center text-white text-2xl m-[14px]">
-        <div className="bg-blue-500 h-[120px] flex-1 ">First use of Tailwind :)</div>
-        <div className="bg-green-500 h-[120px] flex-1">First use of Tailwind :)</div>
+        sm:flex 
+        sm:flex-col 
+      
+        md:flex 
+        md:flex-wrap
+        md:flex-row
+
+        lg:flex-row">
+
+        <div className="
+        bg-red-500 h-[80px] m-1 
+        md:w-full
+        lg:flex-1" >this is a red box :)</div>
+
+        <div className="bg-blue-500 h-[80px] m-1 
+        md:w-1/2
+        md:flex-1
+        lg:flex-1">blue skies ahead :)</div>
+
+        <div className="bg-green-500 h-[80px] m-1 
+        md:w-1/2
+        md:flex-1
+        lg:flex-1">green means go!</div>
       </div>
 
     </>
